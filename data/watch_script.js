49a4ffb75223cb641fdf8037a468dcdc0bbdea13
nralbrecht <nralbrecht@web.de>
2016-05-22 00:32:41 +0200

    Makes controls visible again if the cursor is moved again
    Fixes a problem, where YouTube doesn't make the controls visible when
    moving the mouse.

--- data/watch_script.js
+++ data/watch_script.js
@@ -1,3 +1,5 @@
+var triggered = false;
+
 document.addEventListener('mousemove', function(e) {
 	var m_player = document.getElementById('c4-player');
 	if (m_player == null) {
@@ -7,6 +9,12 @@ document.addEventListener('mousemove', function(e) {
 	if (e.clientX == 0) {
 		if (m_player.className.indexOf(' ytp-autohide') < 0) {
 			m_player.className += ' ytp-autohide';
+			triggered = true;
 		}
+	} else if (triggered && e.clientX > 0) {
+		if (m_player.className.indexOf(' ytp-autohide') >= 0) {
+			m_player.className = m_player.className.replace(/ ytp-autohide/, "");
+		}
+		triggered = false;
 	}
 });
