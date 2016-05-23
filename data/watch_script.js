55a1ae2c8cf614119767e82810573ec374e754f1
nralbrecht <nralbrecht@web.de>
2016-05-23 21:21:39 +0200

    Adds the actionPadding preference
    This uses the "simple-prefs" module to let Firefox manage the
    preferences. The current preferences are given to the content script via
    the "onAttach" event.

--- data/watch_script.js
+++ data/watch_script.js
@@ -1,4 +1,10 @@
 var triggered = false;
+var options = {};
+
+self.port.on("prefs", function(newOptions) {
+	options = newOptions;
+	console.log(options.actionPadding);
+});
 
 document.addEventListener('mousemove', function(e) {
 	var m_player = document.getElementById('c4-player');
@@ -6,12 +12,12 @@ document.addEventListener('mousemove', function(e) {
 		m_player = document.getElementById('movie_player');
 	}
 
-	if (e.clientX == 0) {
+	if (e.clientX <= options.actionPadding) {
 		if (m_player.className.indexOf(' ytp-autohide') < 0) {
 			m_player.className += ' ytp-autohide';
 			triggered = true;
 		}
-	} else if (triggered && e.clientX > 0) {
+	} else if (triggered && e.clientX > options.actionPadding) {
 		if (m_player.className.indexOf(' ytp-autohide') >= 0) {
 			m_player.className = m_player.className.replace(/ ytp-autohide/, "");
 		}
