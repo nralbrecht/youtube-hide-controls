d5c9324b6be20068fa5c4e83d0d54faf414772bc
nralbrecht <nralbrecht@web.de>
2016-05-31 18:29:51 +0200

    Adds a modifiable hot key
    The variable in the options is a integer containing the key code

--- data/watch_script.js
+++ data/watch_script.js
@@ -1,25 +1,36 @@
 var triggered = false;
+var key_was_pressed = false;
 var options = {};
+
+var m_player = document.getElementById('c4-player');
+if (m_player == null) {
+	m_player = document.getElementById('movie_player');
+}
 
 self.port.on("prefs", function(newOptions) {
 	options = newOptions;
 });
 
 document.addEventListener('mousemove', function(e) {
-	var m_player = document.getElementById('c4-player');
-	if (m_player == null) {
-		m_player = document.getElementById('movie_player');
-	}
-
 	if (e.clientX <= options.actionPadding) {
 		if (m_player.className.indexOf(' ytp-autohide') < 0) {
 			m_player.className += ' ytp-autohide';
 			triggered = true;
 		}
-	} else if (triggered && e.clientX > options.actionPadding) {
+	} else if ((triggered && e.clientX > options.actionPadding) || key_was_pressed) {
 		if (m_player.className.indexOf(' ytp-autohide') >= 0) {
 			m_player.className = m_player.className.replace(/ ytp-autohide/, "");
 		}
 		triggered = false;
+		key_was_pressed = false;
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode === options.keyCode) {
+		if (m_player.className.indexOf(' ytp-autohide') < 0) {
+			m_player.className += ' ytp-autohide';
+			key_was_pressed = true;
+		}
 	}
 });
