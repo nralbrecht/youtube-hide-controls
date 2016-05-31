b55e0252b8c4f7b0f6ad9270e2df156cd534c168
nralbrecht <nralbrecht@web.de>
2016-05-31 14:45:01 +0200

    Revert "Revert "Adds the actionPadding preference""
    This reverts commit 3baaade13e03daddc81d35480c13aedda42c5933.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var pageMod = require("sdk/page-mod");
+var options = require("sdk/simple-prefs");
 
 pageMod.PageMod({
   include: [/.*youtube\.com\/watch\?.*/, /.*youtube\.com\/user.*/],
   contentScriptFile: "./watch_script.js"
+  onAttach: function(worker) {
+    worker.port.emit("prefs", options.prefs);
+  }
 });
