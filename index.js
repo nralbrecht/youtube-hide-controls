efa1b1bd149b9d88d8c6da6879ad187ba046427e
nralbrecht <nralbrecht@web.de>
2016-06-04 23:26:17 +0200

    Modifies the page mod to append to existing tabs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var options = require("sdk/simple-prefs");
 pageMod.PageMod({
   include: [/.*youtube\.com\/watch\?.*/, /.*youtube\.com\/user.*/],
   contentScriptFile: "./watch_script.js",
+  attachTo: ["existing", "top"],
   onAttach: function(worker) {
     worker.port.emit("prefs", options.prefs);
   }
