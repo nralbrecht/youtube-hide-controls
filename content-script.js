d97b062317c822e4ecd9c780152a439096ec3d06
nralbrecht <nralbrecht@web.de>
2016-12-24 16:40:13 +1300

    Add hotkey to options page

--- content-script.js
+++ content-script.js
@@ -1,40 +1,58 @@
 let yt_player = null;
 let triggered = false;
 
-let triggerDistance = 5;
+let triggerDistance;
+let useHotkey;
+let hotkey;
 
-chrome.storage.local.get("triggerDistance", function(res) {
-    if (res.triggerDistance === undefined) {
-        triggerDistance = 5;
-
-        chrome.storage.local.set({
-            "triggerDistance": triggerDistance
-        });
-    } else {
-        triggerDistance = res.triggerDistance;
-    }
+chrome.storage.local.get(["triggerDistance", "useHotkey", "hotkey"], function(res) {
+    triggerDistance = res.triggerDistance || 5;
+    useHotkey = res.useHotkey || false;
+    hotkey = res.hotkey || null;
 });
 
 chrome.storage.onChanged.addListener(function(changes) {
     if (changes["triggerDistance"]) {
         triggerDistance = changes["triggerDistance"].newValue;
     }
+    if (changes["useHotkey"]) {
+        useHotkey = changes["useHotkey"].newValue;
+    }
+    if (changes["hotkey"]) {
+        hotkey = changes["hotkey"].newValue;
+    }
 });
+
+function hideControls() {
+    if (yt_player.className.indexOf(" ytp-autohide") < 0) {
+        yt_player.className += " ytp-autohide";
+        yt_player.focus();
+        triggered = true;
+    }
+}
+
+function showControls() {
+    if (yt_player.className.indexOf(" ytp-autohide") >= 0) {
+        yt_player.className = yt_player.className.replace(/ ytp-autohide/, "");
+        triggered = false;
+    }
+}
 
 let mousemove_handler = function(e) {
     if (e.clientX <= triggerDistance) {
-        if (yt_player.className.indexOf(" ytp-autohide") < 0) {
-            yt_player.className += " ytp-autohide";
-            triggered = true;
-            yt_player.focus();
-        }
+        hideControls();
     } else if (triggered && e.clientX > triggerDistance) {
-        if (yt_player.className.indexOf(" ytp-autohide") >= 0) {
-            yt_player.className = yt_player.className.replace(/ ytp-autohide/, "");
-        }
-        triggered = false;
+        showControls();
     }
-};
+}
+
+let keypress_handler = function(e) {
+    if (useHotkey && hotkey) {
+        if (hotkey.shiftKey == e.shiftKey && hotkey.ctrlKey == e.ctrlKey && hotkey.metaKey == e.metaKey && hotkey.altKey == e.altKey && hotkey.key == e.key) {
+            hideControls();
+        }
+    }
+}
 
 let interval_id = setInterval(function() {
     yt_player = document.getElementById("c4-player") || document.getElementById("movie_player");
@@ -42,5 +60,6 @@ let interval_id = setInterval(function() {
     if(yt_player){
         clearInterval(interval_id);
         document.addEventListener("mousemove", mousemove_handler);
+        document.addEventListener("keypress", keypress_handler);
     }
-}, 1000);
+}, 750);
--- options.js
+++ options.js
@@ -1,16 +1,24 @@
-let triggerDistanceInput = document.getElementById('triggerDistance');
+let triggerDistanceInput = document.getElementsByName("triggerDistance")[0];
+let useHotkeyInput = document.getElementsByName("useHotkey")[0];
+let hotkeyInput = document.getElementsByName("hotkey")[0];
 
-chrome.storage.local.get("triggerDistance", function(res) {
-	if (res.triggerDistance !== undefined) {
-		triggerDistanceInput.value = res.triggerDistance;
-	}
-	else {
-		triggerDistanceInput.value = 5;
+function keyToString(e) {
+	if (e.shiftKey == undefined && e.ctrlKey == undefined && e.metaKey == undefined && e.altKey == undefined && e.key == undefined) return false;
 
-		chrome.storage.local.set({
-			"triggerDistance": 5
-		});
-	}
+	let result = (e.ctrlKey)? "Ctrl+" : "";
+	result += (e.shiftKey)? "Shift+" : "";
+	result += (e.altKey)? "Alt+" : "";
+	result += (e.meta)? "Meta+" : "";
+	result += e.key[0].toUpperCase() + e.key.substr(1);
+
+	return result;
+}
+
+chrome.storage.local.get(["triggerDistance", "useHotkey", "hotkey"], function(res) {
+	triggerDistanceInput.value = res.triggerDistance || 5;
+	useHotkeyInput.checked = res.useHotkey || false;
+	if (!useHotkeyInput.checked) hotkeyInput.disabled = true;
+	hotkeyInput.value = keyToString(res.hotkey) || "none";
 });
 
 triggerDistanceInput.addEventListener("input", function(e) {
@@ -19,4 +27,32 @@ triggerDistanceInput.addEventListener("input", function(e) {
 			"triggerDistance": e.target.value
 		});
 	}
+});
+
+useHotkeyInput.addEventListener("click", function(e) {
+	if (e.target.checked) {
+		hotkeyInput.disabled = false;
+	} else {
+		hotkeyInput.disabled = true;
+	}
+
+	chrome.storage.local.set({
+		"useHotkey": e.target.checked
+	});
+});
+
+hotkeyInput.addEventListener("keypress", function(e) {
+	if (e.keyCode < 20 && e.keyCode > 15) return;
+	if (document.activeElement.name !== "hotkey") return;
+
+	let hotkey = {
+		"shiftKey": e.shiftKey,
+		"ctrlKey": e.ctrlKey,
+		"metaKey": e.metaKey,
+		"altKey": e.altKey,
+		"key": e.key
+	}
+	e.preventDefault();
+	e.target.value = keyToString(hotkey);
+	chrome.storage.local.set({ hotkey });
 });
