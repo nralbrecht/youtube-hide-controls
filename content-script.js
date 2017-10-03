905725dc23044eacf9ee09435ac4773468bd7e60
nralbrecht <nralbrecht@web.de>
2017-10-03 17:46:51 +0200

    Make the addon more reliable

--- content-script.js
+++ content-script.js
@@ -1,65 +1,54 @@
-let yt_player = null;
-let triggered = false;
-
-let triggerDistance;
-let useHotkey;
 let hotkey;
+let useHotkey;
+let triggerDistance;
+let isHidden = false;
 
-chrome.storage.local.get(["triggerDistance", "useHotkey", "hotkey"], function(res) {
+browser.storage.local.get(["triggerDistance", "useHotkey", "hotkey"], function(res) {
     triggerDistance = res.triggerDistance || 5;
     useHotkey = res.useHotkey || false;
     hotkey = res.hotkey || null;
 });
 
-chrome.storage.onChanged.addListener(function(changes) {
-    if (changes["triggerDistance"]) {
-        triggerDistance = changes["triggerDistance"].newValue;
+browser.storage.onChanged.addListener(function(changes) {
+    if (changes.triggerDistance) {
+        triggerDistance = changes.triggerDistance.newValue;
     }
-    if (changes["useHotkey"]) {
-        useHotkey = changes["useHotkey"].newValue;
+    if (changes.useHotkey) {
+        useHotkey = changes.useHotkey.newValue;
     }
-    if (changes["hotkey"]) {
-        hotkey = changes["hotkey"].newValue;
+    if (changes.hotkey) {
+        hotkey = changes.hotkey.newValue;
     }
 });
 
 function hideControls() {
-    if (yt_player.className.indexOf(" ytp-autohide") < 0) {
-        yt_player.className += " ytp-autohide";
-        yt_player.focus();
-        triggered = true;
-    }
+    var player = document.getElementsByTagName("video")[0].parentElement.parentElement.wrappedJSObject;
+
+    player.hideControls();
+    player.style.cursor = "none";
+    isHidden = true;
 }
 
 function showControls() {
-    if (yt_player.className.indexOf(" ytp-autohide") >= 0) {
-        yt_player.className = yt_player.className.replace(/ ytp-autohide/, "");
-        triggered = false;
-    }
+    var player = document.getElementsByTagName("video")[0].parentElement.parentElement.wrappedJSObject;
+
+    player.showControls();
+    player.style.cursor = "";
+    isHidden = false;
 }
 
-let mousemove_handler = function(e) {
+document.addEventListener("mousemove", function(e) {
     if (e.clientX <= triggerDistance) {
         hideControls();
-    } else if (triggered && e.clientX > triggerDistance) {
+    } else if (isHidden && e.clientX > triggerDistance) {
         showControls();
     }
-}
+});
 
-let keypress_handler = function(e) {
+document.addEventListener("keypress", function(e) { 
     if (useHotkey && hotkey) {
         if (hotkey.shiftKey == e.shiftKey && hotkey.ctrlKey == e.ctrlKey && hotkey.metaKey == e.metaKey && hotkey.altKey == e.altKey && hotkey.key == e.key) {
             hideControls();
         }
     }
-}
-
-let interval_id = setInterval(function() {
-    yt_player = document.getElementById("c4-player") || document.getElementById("movie_player");
-
-    if(yt_player){
-        clearInterval(interval_id);
-        document.addEventListener("mousemove", mousemove_handler);
-        document.addEventListener("keypress", keypress_handler);
-    }
-}, 750);
+});
