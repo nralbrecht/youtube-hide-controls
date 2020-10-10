4261d2063aa9b0026f81e545e20eac33514c6d52
nralbrecht <nralbrecht@web.de>
2020-10-10 14:28:44 +0200

    Add editor config and change tabs to spaces

--- options/options.js
+++ options/options.js
@@ -8,44 +8,44 @@ let triggerRight = document.querySelector("#right");
 let triggerBottom = document.querySelector("#bottom");
 
 function keyToString(e) {
-	if (e.shiftKey == e.ctrlKey == e.metaKey == e.altKey == e.key == undefined) {
-		return false;
-	}
+    if (e.shiftKey == e.ctrlKey == e.metaKey == e.altKey == e.key == undefined) {
+        return false;
+    }
 
-	let result = "";
-	if (e.ctrlKey) result += "Ctrl+";
-	if (e.shiftKey) result += "Shift+";
-	if (e.altKey) result += "Alt+";
-	if (e.meta) result += "Meta+";
-	if (e.code.startsWith("Numpad")) result += "Numpad ";
-	result += e.key[0].toUpperCase() + e.key.substr(1);
+    let result = "";
+    if (e.ctrlKey) result += "Ctrl+";
+    if (e.shiftKey) result += "Shift+";
+    if (e.altKey) result += "Alt+";
+    if (e.meta) result += "Meta+";
+    if (e.code.startsWith("Numpad")) result += "Numpad ";
+    result += e.key[0].toUpperCase() + e.key.substr(1);
 
-	return result;
+    return result;
 }
 
 chrome.storage.local.get(["triggerTop", "triggerLeft", "triggerRight", "triggerBottom", "useHotkey", "hotkey", "invertTrigger"], function (res) {
-	triggerTop.value = res.triggerTop || -1;
-	triggerLeft.value = res.triggerLeft || 5;
-	triggerRight.value = res.triggerRight || 5;
-	triggerBottom.value = res.triggerBottom || -1;
+    triggerTop.value = res.triggerTop || -1;
+    triggerLeft.value = res.triggerLeft || 5;
+    triggerRight.value = res.triggerRight || 5;
+    triggerBottom.value = res.triggerBottom || -1;
 
-	useHotkeyInput.checked = res.useHotkey || false;
-	if (!useHotkeyInput.checked) {
-		hotkeyInput.disabled = true;
-	}
+    useHotkeyInput.checked = res.useHotkey || false;
+    if (!useHotkeyInput.checked) {
+        hotkeyInput.disabled = true;
+    }
 
-	hotkeyInput.value = res.hotkey? keyToString(res.hotkey) : "none";
+    hotkeyInput.value = res.hotkey? keyToString(res.hotkey) : "none";
 
-	invertTrigger.checked = res.invertTrigger || false;
+    invertTrigger.checked = res.invertTrigger || false;
 });
 
 function updateTrigger(e) {
-	if (e.target.validity.valid) {
-		let temp = {};
-		temp["trigger" + e.target.name] = e.target.value;
-		console.log("insert: ", temp);
-		chrome.storage.local.set(temp);
-	}
+    if (e.target.validity.valid) {
+        let temp = {};
+        temp["trigger" + e.target.name] = e.target.value;
+        console.log("insert: ", temp);
+        chrome.storage.local.set(temp);
+    }
 }
 
 triggerTop.addEventListener("input", updateTrigger);
@@ -54,38 +54,38 @@ triggerRight.addEventListener("input", updateTrigger);
 triggerBottom.addEventListener("input", updateTrigger);
 
 useHotkeyInput.addEventListener("click", function(e) {
-	if (e.target.checked) {
-		hotkeyInput.disabled = false;
-	} else {
-		hotkeyInput.disabled = true;
-	}
+    if (e.target.checked) {
+        hotkeyInput.disabled = false;
+    } else {
+        hotkeyInput.disabled = true;
+    }
 
-	chrome.storage.local.set({
-		"useHotkey": useHotkeyInput.checked
-	});
+    chrome.storage.local.set({
+        "useHotkey": useHotkeyInput.checked
+    });
 });
 
 hotkeyInput.addEventListener("keydown", function(e) {
-	if (e.keyCode < 20 && e.keyCode > 15) return;
-	if (e.target.name !== "hotkey") return;
+    if (e.keyCode < 20 && e.keyCode > 15) return;
+    if (e.target.name !== "hotkey") return;
 
-	let hotkey = {
-		"shiftKey": e.shiftKey,
-		"ctrlKey": e.ctrlKey,
-		"metaKey": e.metaKey,
-		"altKey": e.altKey,
-		"code": e.code,
-		"key": e.key
-	}
+    let hotkey = {
+        "shiftKey": e.shiftKey,
+        "ctrlKey": e.ctrlKey,
+        "metaKey": e.metaKey,
+        "altKey": e.altKey,
+        "code": e.code,
+        "key": e.key
+    }
 
-	e.target.value = keyToString(hotkey);
-	chrome.storage.local.set({ hotkey });
+    e.target.value = keyToString(hotkey);
+    chrome.storage.local.set({ hotkey });
 });
 
 invertTrigger.addEventListener("click", function(e) {
-	chrome.storage.local.set({
-		"invertTrigger": invertTrigger.checked
-	});
+    chrome.storage.local.set({
+        "invertTrigger": invertTrigger.checked
+    });
 });
 
 document.querySelector("#triggerDistance .preferences-title").innerText = chrome.i18n.getMessage("triggerDistanceOptionTitle");
